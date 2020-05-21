19853551e3f3f5ca91ddfee70555f6cec1c09bf5
Juan Guillen <juanguillenmtz16@gmail.com>
2020-05-20 19:14:45 -0500

    make it better

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "kelywfwclfcuqt"); // Usuario
+define('DB_PASSWORD', "018ba14302dc3f6f17224f8dbf65995c8141d6c0da4db9a7b74b05b763c51024"); // Password
+define('DB_DATABASE', "dds4m144p2g95d"); // Nombre de la base de datos
+define('DB_SERVER', "ec2-35-171-31-33.compute-1.amazonaws.com"); //host server
+?>
